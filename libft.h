76a2e0fed17fd6488010656ed8a76a6e7e0dfffd
Oleh Ohnivchuk <oohnivch@c3r4p1.42vienna.com>
2024-04-17 17:06:05 +0200

    header fix

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: oohnivch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:21:39 by oohnivch          #+#    #+#             */
-/*   Updated: 2024/04/16 19:03:28 by oohnivch         ###   ########.fr       */
+/*   Updated: 2024/04/17 17:05:43 by oohnivch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,15 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-
+//	Char
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-int		ft_isspace(int c);
-
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t n);
-void	*ft_memchr(const void *str, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void	*dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *str, int c, size_t n);
-
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+//	String
 int		ft_atoi(const char *str);
 char	*ft_itoa(int n);
 char	**ft_split(const char *s, char c);
@@ -48,20 +41,26 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strtrim(const char *s1, const char *set);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-
+//	Memory
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void	*dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *str, int c, size_t n);
+//	File
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
+//	Struct for bonus
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-
+//	Bonus
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
